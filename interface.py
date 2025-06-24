0534a59f4e68dc61a0165674452dcb6304d78d2b
unknown <felipemachadopereira2@gmail.com>
2025-06-24 11:01:14 -0300

    [Add] Interactions added

--- interface.py
+++ interface.py
@@ -1,6 +1,12 @@
+# Functionality Imports
+from PySide6.QtCore import QThread, Signal, QSettings
+from PySide6.QtWidgets import QFileDialog
+import yt_dlp
+
+# Interface Imports
 from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
     QMetaObject, QObject, QPoint, QRect,
-    QSize, QTime, QUrl, Qt, QThread, Signal)
+    QSize, QTime, QUrl, Qt)
 from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QFont, QFontDatabase, QGradient, QIcon,
     QImage, QKeySequence, QLinearGradient, QPainter,
@@ -9,23 +15,46 @@ from PySide6.QtWidgets import (QApplication, QCheckBox, QComboBox, QFormLayout,
     QFrame, QGridLayout, QHBoxLayout, QLabel,
     QLineEdit, QMainWindow, QMenuBar, QPlainTextEdit,
     QProgressBar, QPushButton, QSizePolicy, QSpacerItem,
-    QStatusBar, QWidget, QFileDialog)
-import yt_dlp
+    QStatusBar, QWidget)
 
 class Ui_MainWindow(object):
+    # setupUi
     def setupUi(self, MainWindow):
         if not MainWindow.objectName():
             MainWindow.setObjectName(u"MainWindow")
-        MainWindow.resize(500, 600)
+        MainWindow.resize(569, 600)
         MainWindow.setToolTipDuration(-1)
         MainWindow.setAutoFillBackground(False)
         MainWindow.setToolButtonStyle(Qt.ToolButtonStyle.ToolButtonIconOnly)
         MainWindow.setDocumentMode(False)
-
         self.centralwidget = QWidget(MainWindow)
         self.centralwidget.setObjectName(u"centralwidget")
         self.gridLayout_2 = QGridLayout(self.centralwidget)
         self.gridLayout_2.setObjectName(u"gridLayout_2")
+        self.horizontalLayout_2 = QHBoxLayout()
+        self.horizontalLayout_2.setObjectName(u"horizontalLayout_2")
+        self.pathLabel = QLabel(self.centralwidget)
+        self.pathLabel.setObjectName(u"pathLabel")
+        font = QFont()
+        font.setFamilies([u"Consolas"])
+        self.pathLabel.setFont(font)
+
+        self.horizontalLayout_2.addWidget(self.pathLabel)
+
+        self.downloadPath = QLineEdit(self.centralwidget)
+        self.downloadPath.setObjectName(u"downloadPath")
+
+        self.horizontalLayout_2.addWidget(self.downloadPath)
+
+        self.pathButton = QPushButton(self.centralwidget)
+        self.pathButton.setObjectName(u"pathButton")
+        self.pathButton.setFont(font)
+
+        self.horizontalLayout_2.addWidget(self.pathButton)
+
+
+        self.gridLayout_2.addLayout(self.horizontalLayout_2, 2, 0, 1, 1)
+
         self.horizontalLayout = QHBoxLayout()
         self.horizontalLayout.setObjectName(u"horizontalLayout")
         self.horizontalSpacer = QSpacerItem(40, 20, QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Minimum)
@@ -39,11 +68,11 @@ class Ui_MainWindow(object):
         sizePolicy.setVerticalStretch(0)
         sizePolicy.setHeightForWidth(self.titleLabel.sizePolicy().hasHeightForWidth())
         self.titleLabel.setSizePolicy(sizePolicy)
-        font = QFont()
-        font.setFamilies([u"Consolas"])
-        font.setPointSize(24)
-        font.setBold(True)
-        self.titleLabel.setFont(font)
+        font1 = QFont()
+        font1.setFamilies([u"Consolas"])
+        font1.setPointSize(24)
+        font1.setBold(True)
+        self.titleLabel.setFont(font1)
         self.titleLabel.setContextMenuPolicy(Qt.ContextMenuPolicy.NoContextMenu)
 
         self.horizontalLayout.addWidget(self.titleLabel)
@@ -57,33 +86,9 @@ class Ui_MainWindow(object):
 
         self.urlTextBox = QPlainTextEdit(self.centralwidget)
         self.urlTextBox.setObjectName(u"urlTextBox")
-        font1 = QFont()
-        font1.setFamilies([u"Consolas"])
-        self.urlTextBox.setFont(font1)
+        self.urlTextBox.setFont(font)
 
         self.gridLayout_2.addWidget(self.urlTextBox, 1, 0, 1, 1)
-
-        self.horizontalLayout_2 = QHBoxLayout()
-        self.horizontalLayout_2.setObjectName(u"horizontalLayout_2")
-        self.pathLabel = QLabel(self.centralwidget)
-        self.pathLabel.setObjectName(u"pathLabel")
-        self.pathLabel.setFont(font1)
-
-        self.horizontalLayout_2.addWidget(self.pathLabel)
-
-        self.downloadPath = QLineEdit(self.centralwidget)
-        self.downloadPath.setObjectName(u"downloadPath")
-
-        self.horizontalLayout_2.addWidget(self.downloadPath)
-
-        self.pathButton = QPushButton(self.centralwidget)
-        self.pathButton.setObjectName(u"pathButton")
-        self.pathButton.setFont(font1)
-
-        self.horizontalLayout_2.addWidget(self.pathButton)
-
-
-        self.gridLayout_2.addLayout(self.horizontalLayout_2, 2, 0, 1, 1)
 
         self.frame = QFrame(self.centralwidget)
         self.frame.setObjectName(u"frame")
@@ -113,7 +118,7 @@ class Ui_MainWindow(object):
         self.videoQualities.addItem("")
         self.videoQualities.addItem("")
         self.videoQualities.setObjectName(u"videoQualities")
-        self.videoQualities.setFont(font1)
+        self.videoQualities.setFont(font)
         self.videoQualities.setEditable(False)
 
         self.formLayout.setWidget(1, QFormLayout.ItemRole.LabelRole, self.videoQualities)
@@ -166,7 +171,7 @@ class Ui_MainWindow(object):
         self.horizontalLayout_4.addItem(self.horizontalSpacer_5)
 
 
-        self.gridLayout_2.addWidget(self.frame, 3, 0, 1, 1)
+        self.gridLayout_2.addWidget(self.frame, 3, 0, 1, 2)
 
         self.progressBar = QProgressBar(self.centralwidget)
         self.progressBar.setObjectName(u"progressBar")
@@ -181,17 +186,30 @@ class Ui_MainWindow(object):
         font4.setFamilies([u"Consolas"])
         font4.setBold(True)
         self.progressBar.setFont(font4)
-        self.progressBar.setValue(0)
+        self.progressBar.setAutoFillBackground(False)
+        self.progressBar.setValue(50)
         self.progressBar.setTextVisible(True)
         self.progressBar.setInvertedAppearance(False)
-        self.progressBar.setHidden(True)
 
-        self.gridLayout_2.addWidget(self.progressBar, 4, 0, 1, 1)
+        self.gridLayout_2.addWidget(self.progressBar, 5, 0, 1, 2)
+
+        self.downloadProgressLabel = QLabel(self.centralwidget)
+        self.downloadProgressLabel.setObjectName(u"downloadProgressLabel")
+        self.downloadProgressLabel.setEnabled(True)
+        sizePolicy.setHeightForWidth(self.downloadProgressLabel.sizePolicy().hasHeightForWidth())
+        self.downloadProgressLabel.setSizePolicy(sizePolicy)
+        self.downloadProgressLabel.setMinimumSize(QSize(0, 0))
+        self.downloadProgressLabel.setTabletTracking(False)
+        self.downloadProgressLabel.setLayoutDirection(Qt.LayoutDirection.RightToLeft)
+        self.downloadProgressLabel.setAlignment(Qt.AlignmentFlag.AlignRight|Qt.AlignmentFlag.AlignTrailing|Qt.AlignmentFlag.AlignVCenter)
+        self.downloadProgressLabel.setWordWrap(False)
+
+        self.gridLayout_2.addWidget(self.downloadProgressLabel, 6, 0, 1, 1)
 
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QMenuBar(MainWindow)
         self.menubar.setObjectName(u"menubar")
-        self.menubar.setGeometry(QRect(0, 0, 500, 33))
+        self.menubar.setGeometry(QRect(0, 0, 569, 33))
         MainWindow.setMenuBar(self.menubar)
         self.statusbar = QStatusBar(MainWindow)
         self.statusbar.setObjectName(u"statusbar")
@@ -201,37 +219,31 @@ class Ui_MainWindow(object):
 
         QMetaObject.connectSlotsByName(MainWindow)
 
-        self.pathButton.clicked.connect(self.downloadDirectory)
-        self.downloadButton.clicked.connect(self.startDownload)
-    # setupUi
-
     # retranslateUi
     def retranslateUi(self, MainWindow):
         MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"Simple YT-DLP", None))
 #if QT_CONFIG(tooltip)
         MainWindow.setToolTip("")
 #endif // QT_CONFIG(tooltip)
+        self.pathLabel.setText(QCoreApplication.translate("MainWindow", u"Download Folder", None))
+#if QT_CONFIG(statustip)
+        self.downloadPath.setStatusTip(QCoreApplication.translate("MainWindow", u"Destination where the videos are gona be downloaded", None))
+#endif // QT_CONFIG(statustip)
+        self.downloadPath.setText("")
+        self.downloadPath.setPlaceholderText(QCoreApplication.translate("MainWindow", u"Download Path", None))
+        self.pathButton.setText(QCoreApplication.translate("MainWindow", u"Change", None))
         self.titleLabel.setText(QCoreApplication.translate("MainWindow", u"SIMPLE YT-DLP", None))
 #if QT_CONFIG(statustip)
         self.urlTextBox.setStatusTip(QCoreApplication.translate("MainWindow", u"Insert URLs here.", None))
 #endif // QT_CONFIG(statustip)
         self.urlTextBox.setPlaceholderText(QCoreApplication.translate("MainWindow", u"Paste your URLs here. Exemple: <URL_1> <URL_2> <URL_3>...", None))
-
-        self.pathLabel.setText(QCoreApplication.translate("MainWindow", u"Download Folder", None))
-#if QT_CONFIG(statustip)
-        self.downloadPath.setStatusTip(QCoreApplication.translate("MainWindow", u"Destination where the videos are gona be downloaded", None))
-#endif // QT_CONFIG(statustip)
-        self.downloadPath.setPlaceholderText(QCoreApplication.translate("MainWindow", u"Download Path", None))
-
-        self.pathButton.setText(QCoreApplication.translate("MainWindow", u"Change", None))
-
         self.videoQualities.setItemText(0, QCoreApplication.translate("MainWindow", u"360p", None))
         self.videoQualities.setItemText(1, QCoreApplication.translate("MainWindow", u"480p", None))
         self.videoQualities.setItemText(2, QCoreApplication.translate("MainWindow", u"720p", None))
         self.videoQualities.setItemText(3, QCoreApplication.translate("MainWindow", u"1080p", None))
         self.videoQualities.setItemText(4, QCoreApplication.translate("MainWindow", u"1440p", None))
         self.videoQualities.setItemText(5, QCoreApplication.translate("MainWindow", u"2160p", None))
-        self.videoQualities.setCurrentIndex(-1)
+
         self.videoQualities.setPlaceholderText(QCoreApplication.translate("MainWindow", u"Video Quality", None))
 #if QT_CONFIG(tooltip)
         self.isPlaylist.setToolTip(QCoreApplication.translate("MainWindow", u"Select to download all the playlist that the video is part of.", None))
@@ -254,7 +266,17 @@ class Ui_MainWindow(object):
         self.downloadButton.setStatusTip(QCoreApplication.translate("MainWindow", u"Download all the URLs present above.", None))
 #endif // QT_CONFIG(statustip)
         self.downloadButton.setText(QCoreApplication.translate("MainWindow", u"DOWNLOAD", None))
+        self.downloadProgressLabel.setText(QCoreApplication.translate("MainWindow", u"TextLabel", None))
     
+    # Setup Functions
+    def setupFunctions(self):
+        # Make buttons work
+        self.downloadButton.clicked.connect(self.startDownload)
+        self.pathButton.clicked.connect(self.downloadDirectory)
+
+        # Show placeholder text
+        self.videoQualities.setCurrentIndex(-1)
+
     # Message Box    
     def show_message(self, message, sucess):
         from PySide6.QtWidgets import QMessageBox
@@ -263,12 +285,21 @@ class Ui_MainWindow(object):
         else:
             QMessageBox.information(None, "Error", message)
 
-    # File Explorer
+    # Select and save download directory
     def downloadDirectory(self):
         directory = QFileDialog.getExistingDirectory(None, "Select Directory")
         if directory:
             self.downloadPath.setText(directory)
 
+            settings = QSettings("YourCompany","Simple YT-DLP")
+            settings.setValue("download_directory", directory)
+    
+    # Load download directory
+    def loadDownloadDirectory(self):
+        settings = QSettings("YourCompany","Simple YT-DLP")
+        directory = settings.value("download_directory", "")
+        self.downloadPath.setText(directory)
+            
     # Download Button Function
     def startDownload(self):
         url_text = self.urlTextBox.toPlainText().strip()
@@ -277,33 +308,40 @@ class Ui_MainWindow(object):
             return
         
         url = url_text.split()
+        qUrl = url_text.count("youtube")
         audio_only = self.isAudioOnly.isChecked()
         playlist = self.isPlaylist.isChecked()
         quality = self.videoQualities.currentText()
+        dPath = self.downloadPath.text()
 
         self.progressBar.setValue(0)
         self.progressBar.setHidden(False)
+        self.downloadProgressLabel.setHidden(False)
+        
 
-        self.thread = downloadThread(url, audio= audio_only, playlist= playlist, quality= quality)
+        self.thread = downloadThread(url, qUrl,audio= audio_only, playlist= playlist, quality= quality, dPath= dPath)
         self.thread.progress_changed.connect(self.progressBar.setValue)
         self.thread.finished.connect(self.show_message)
-        self.thread.pDisplay.connect(self.progressBar.setHidden)
-        self.thread.dPath.connect(self.downloadPath)
+        self.thread.pBarDisplay.connect(self.progressBar.setHidden)
+        self.thread.pLabelDisplay.connect(self.downloadProgressLabel.setHidden)
         self.thread.start()
 
 # Download Class
 class downloadThread(QThread):
     progress_changed = Signal(int)
     finished = Signal(str, bool)
-    pDisplay = Signal(bool)
-    dPath = Signal(str)
+    pBarDisplay = Signal(bool)
+    pLabelDisplay = Signal(bool)
+    dCount = 0
 
-    def __init__(self, url, audio=True, quality="best", playlist=False):
+    def __init__(self, url, qUrl,audio=True, quality="best", playlist=False, dPath=''):
         super().__init__()
         self.url = url
+        self.qUrl = qUrl
         self.audio = audio
         self.quality = quality
         self.playlist = playlist
+        self.dPath = dPath
 
     def run(self):
         def progress_hook(d):
@@ -315,6 +353,7 @@ class downloadThread(QThread):
                     self.progress_changed.emit(percent)
             elif d['status'] == 'finished':
                 self.progress_changed.emit(100)
+                self.dCount += 1
 
         try:        
             if self.audio:
@@ -352,7 +391,8 @@ class downloadThread(QThread):
             with yt_dlp.YoutubeDL(ydl_opts) as ydl:
                 ydl.download(self.url)
             self.finished.emit("Download Completed!", True)
-            self.pDisplay.emit(True)
+            self.pBarDisplay.emit(True)
+            self.pLabelDisplay.emit(True)
         except Exception as e:
             self.finished.emit(str(e), False)
 
@@ -364,5 +404,9 @@ if __name__ == "__main__":
     MainWindow = QMainWindow()
     ui = Ui_MainWindow()
     ui.setupUi(MainWindow)
+    ui.setupFunctions()
+    ui.loadDownloadDirectory()
+    ui.progressBar.setHidden(True)
+    ui.downloadProgressLabel.setHidden(True)
     MainWindow.show()
     sys.exit(app.exec())
